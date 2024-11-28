5de74b94f5796824c3ccfd5428b7c98d95e6f6c8
MiguelEM <jose.miguel.7897763@gmail.com>
2024-11-28 01:24:29 -0600

    new dashboard

--- Pagina/pages/dashboard.php
+++ Pagina/pages/dashboard.php
@@ -1,79 +1,19 @@
-<?php
-require_once("./Conexion/Tabla_paciente.php");
-$idPaciente = 1;
-//Instancia modelo
-$tabla_paciente = new Tabla_paciente();
-$data = $tabla_paciente->GetPaciente($idPaciente);
-//print_r($data);
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-  <title>Dashboard</title>
-  <meta charset="utf-8" />
-  <meta name="author" content="Rodrigo" />
-  <meta name="description" content="Pagina para dentista" />
-  <meta name="keywords" content="HTML, CSS, JavaScript, React" />
-  <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <!-- basic -->
-  <meta charset="utf-8" />
+  <meta charset="UTF-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-
-  <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" />
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" media="screen" />
-
-
-
-  <!-- Estilos Personalizados -->
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Dashboard</title>
+  <!--Boostrap 5.3-->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <!--CSS personalizado-->
+  <link rel="stylesheet" href="style.css" />
+  <!-- Fontawesome CDN Link -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" />
   <style>
-    /* Panel lateral fijo */
-    .sidepanel {
-      width: 250px;
-      height: 100vh;
-      /* Altura completa */
-      position: fixed;
-      background-color: #111;
-      padding-top: 20px;
-      color: white;
-    }
-
-    .sidepanel a {
-      padding: 10px 15px;
-      text-decoration: none;
-      font-size: 18px;
-      color: white;
-      display: block;
-      transition: 0.3s;
-    }
-
-    .sidepanel a:hover {
-      background-color: #575757;
-    }
-
-    /* Contenido principal ajustado */
-    .main-content {
-      margin-left: 250px;
-      /* Deja espacio para el panel lateral */
-    }
-
-    /* Estilo del encabezado */
-    .header {
-      background-color: #575757;
-      padding: 10px;
-      color: white;
-    }
-
-    .content {
-      flex-grow: 1;
-      padding: 20px;
-      background: #f4f4f4;
-    }
-
     .section {
       display: none;
     }
@@ -97,193 +37,206 @@ $data = $tabla_paciente->GetPaciente($idPaciente);
       outline: none;
       box-shadow: none;
     }
+
+    .collapse {
+      transition: height 0.5s ease;
+      /* Aumenta el tiempo y suaviza la animación */
+    }
+
+    /* Transición suave para el ícono */
+    .icon-rotate {
+      transition: transform 0.3s ease;
+      /* Suaviza la rotación */
+    }
+
+    .rotate-up {
+      transform: rotate(180deg);
+      /* Rota el ícono 180 grados */
+    }
   </style>
+
 </head>
 
 <body>
-  <!-- Sidebar fijo -->
-  <div class="sidepanel">
-    <h2 class="text-center">Dashboard</h2>
-    <a href="../index.php">Inicio</a>
-    <ul>
-      <li><a onclick="showSection('pacientes')">Pacientes</a></li>
-      <li><a onclick="showSection('historiales')">Historiales</a></li>
-      <li><a onclick="showSection('antecedentes')">Antecedentes</a></li>
-    </ul>
-  </div>
+  <!--Sidebard-->
+  <nav class="sidebar">
+    <a href="#" class="logo">Dashboard</a>
 
-  <!-- Contenido Principal -->
-  <div class="main-content">
-    <!-- Encabezado -->
-    <div class="header">
-      <h1>Panel de Control</h1>
+    <div class="menu-content">
+      <ul class="menu-items">
+
+        <li class="item">
+          <a onclick="showSection('pacientes')">Pacientes</a>
+        </li>
+        <li class="item">
+          <a onclick="showSection('historiales')">Historiales</a>
+        </li>
+        <li class="item">
+          <a onclick="showSection('antecedentes')">Antecedentes</a>
+        </li>
+
+        <!--
+        <li class="item">
+          <a href="#">Your first link</a>
+        </li>
+
+        <li class="item">
+          <div class="submenu-item">
+            <span>First submenu</span>
+            <i class="fa-solid fa-chevron-right"></i>
+          </div>
+
+          <ul class="menu-items submenu">
+            <div class="menu-title">
+              <i class="fa-solid fa-chevron-left"></i>
+              Your submenu title
+            </div>
+            <li class="item">
+              <a href="#">First sublink</a>
+            </li>
+            <li class="item">
+              <a href="#">First sublink</a>
+            </li>
+            <li class="item">
+              <a href="#">First sublink</a>
+            </li>
+          </ul>
+        </li>
+  -->
+      </ul>
+    </div>
+  </nav>
+
+  <nav class="navbar">
+    <i class="fa-solid fa-bars" id="sidebar-close"></i>
+  </nav>
+
+  <div class="main">
+    <div id="pacientes" class="section">
+      <h2>Gestión de Pacientes</h2>
+      <p>Contenido relacionado con los pacientes aquí.</p>
     </div>
 
-    <!-- Secciones de Contenido -->
-    <div class="container mt-4">
+    <div id="historiales" class="section active">
+      <h2>Gestión de Historiales</h2>
+
+      <br> <!--datos personales del paciente -->
+
       <div class="row">
-        <!-- Tarjeta 1: Total Pacientes -->
-        <div class="col-md-4">
-          <div class="card text-white bg-info mb-3">
-            <div class="card-body">
-              <h5 class="card-title">Total Pacientes</h5>
-              <p class="card-text">120</p>
-            </div>
-          </div>
-        </div>
-
-        <!-- Tarjeta 2: Citas Pendientes -->
-        <div class="col-md-4">
-          <div class="card text-white bg-warning mb-3">
-            <div class="card-body">
-              <h5 class="card-title">Citas Pendientes</h5>
-              <p class="card-text">15</p>
-            </div>
-          </div>
-        </div>
-
-        <!-- Tarjeta 3: Nuevas Consultas -->
-        <div class="col-md-4">
-          <div class="card text-white bg-success mb-3">
-            <div class="card-body">
-              <h5 class="card-title">Nuevas Consultas</h5>
-              <p class="card-text">30</p>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <!-- Contenido dinámico -->
-      <div class="content">
-        <div id="pacientes" class="section">
-          <h2>Gestión de Pacientes</h2>
-          <p>Contenido relacionado con los pacientes aquí.</p>
-        </div>
-
-        <div id="historiales" class="section active">
-          <h2>Gestión de Historiales</h2>
-
-          <br> <!--datos personales del paciente -->
-
-          <div class="row m-1">
-            <div class="col-12 bg-white">
-              <form>
-                <legend style="text-align: center;">Datos personales</legend>
-                <div class="row">
-                  <div class="form-group col">
-                    <label for="nombre">Nombre</label>
-                    <input type="text" class="form-control" id="nombre">
-                  </div>
-                  <div class="form-group col">
-                    <label for="ap">Apellido paterno</label>
-                    <input type="text" class="form-control" id="ap">
-                  </div>
-                  <div class="form-group col">
-                    <label for="am">Apellido materno</label>
-                    <input type="text" class="form-control" id="am">
-                  </div>
-                </div>
-
-                <div class="row">
-                  <div class="form-group col-3">
-                    <label for="fn">Fecha de Nacimiento</label>
-                    <input type="date" class="form-control" id="fn">
-                  </div>
-                  <div class="form-group col-3">
-                    <label for="fn">Teléfono</label>
-                    <input type="tel" class="form-control" id="fn">
-                  </div>
-                  <div class="form-group col">
-                    <label for="mail">Correo electrónico</label>
-                    <input type="email" class="form-control" id="mail">
-                  </div>
-                </div>
-
-                <div class="row">
-                  <div class="form-group col mb-3">
-                    <label for="calle">Calle y número</label>
-                    <input type="text" class="form-control" id="calle">
-                  </div>
-                  <div class="form-group col">
-                    <label for="col">Colonia</label>
-                    <input type="text" class="form-control" id="col">
-                  </div>
-                  <div class="form-group col">
-                    <label for="municipio">Municipio</label>
-                    <input type="text" class="form-control" id="municipio">
-                  </div>
-                  <div class="form-group col">
-                    <label for="estado">Estado</label>
-                    <input type="text" class="form-control" id="estado">
-                  </div>
-                </div>
-              </form>
-            </div>
-          </div> <!--Fin datos del paciente-->
-
-          <br> <!-- antecedentes Patologicos -->
-          <div class="row m-1">
-            <div class="col-12 bg-white p-3">
-              <!-- Botón que activa el panel -->
-              <button class="btn legend-button btn-block" type="button" data-toggle="collapse" data-target="#antecedentesPanel" aria-expanded="false" aria-controls="antecedentesPanel">
-                <i class="fas fa-angle-down" style="font-size: 1.5rem;"></i> Antecedentes Patológicos
-              </button>
-
-              <!-- Panel colapsable -->
-              <div class=" mt-3" id="antecedentesPanel">
-                <form id="pacienteForm">
-                  <div class="row">
-
-                    <div class="form-group col-5">
-                      <label for="diabetes">Padecimiento</label>
-                      <input type="text" class="form-control" id="diabetes" value="Diabetes" readonly>
-                    </div>
-
-                    <!-- Nueva sección para el checkbox -->
-                    <div class="form-group col-1">
-                      <label for="estadoCheckbox">Estado</label>
-                      <div class="d-flex align-items-center" style="height: 15%; padding-left: 8px;">
-                        <div class="form-check m-0">
-                          <input type="checkbox" class="form-check-input" id="estadoCheckbox" style="transform: scale(1.5);" checked disabled>
-                        </div>
-                      </div>
-                    </div>
-
-
-                    <!-- Nueva sección para el input de texto -->
-                    <div class="form-group col-6">
-                      <label for="descripcion">Descripción</label>
-                      <input type="text" class="form-control" id="descripcion" placeholder="Descripción" readonly>
-                    </div>
-
-                  </div>
-                </form>
+        <div class="col bg-white">
+          <form>
+            <legend class="mt-3 mb-3" style="text-align: center;">Datos personales</legend>
+            <div class="row mb-3">
+              <div class="form-group col">
+                <label for="nombre">Nombre</label>
+                <input type="text" class="form-control" id="nombre"
+                  style="cursor:default ; outline: none; box-shadow: none;" value="Jose Miguel" readonly>
+              </div>
+              <div class="form-group col">
+                <label for="ap">Apellido paterno</label>
+                <input type="text" class="form-control" id="ap"
+                  style="cursor:default ; outline: none; box-shadow: none;" value="Echavarria" readonly>
+              </div>
+              <div class="form-group col">
+                <label for="am">Apellido materno</label>
+                <input type="text" class="form-control" id="am"
+                  style="cursor:default ; outline: none; box-shadow: none;" value="Matamoros" readonly>
               </div>
             </div>
+
+            <div class="row mb-3">
+              <div class="form-group col-3">
+                <label for="fn">Fecha de Nacimiento</label>
+                <input type="date" class="form-control" id="fn" value="2024-05-01" readonly>
+              </div>
+              <div class="form-group col-3">
+                <label for="fn">Teléfono</label>
+                <input type="tel" class="form-control" id="fn" style="cursor:default ; outline: none; box-shadow: none;"
+                  value="2461987568" readonly>
+              </div>
+              <div class="form-group col">
+                <label for="mail">Correo electrónico</label>
+                <input type="email" class="form-control" id="mail"
+                  style="cursor:default ; outline: none; box-shadow: none;" value="jose.migue.7897763@gmail.com"
+                  readonly>
+              </div>
+            </div>
+
+            <div class="row">
+              <div class="form-group col mb-4">
+                <label for="calle">Calle y número</label>
+                <input type="text" class="form-control" id="calle"
+                  style="cursor:default ; outline: none; box-shadow: none;" value="3a Privada de independencia #12"
+                  readonly>
+              </div>
+              <div class="form-group col">
+                <label for="col">Colonia</label>
+                <input type="text" class="form-control" id="col"
+                  style="cursor:default ; outline: none; box-shadow: none;" value="Ocotlan" readonly>
+              </div>
+              <div class="form-group col">
+                <label for="municipio">Municipio</label>
+                <input type="text" class="form-control" id="municipio"
+                  style="cursor:default ; outline: none; box-shadow: none;" value="Tlaxcala de Xicohténcatl" readonly>
+              </div>
+              <div class="form-group col">
+                <label for="estado">Estado</label>
+                <input type="text" class="form-control" id="estado"
+                  style="cursor:default ; outline: none; box-shadow: none;" value="Tlaxcala" readonly>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div> <!--Fin datos del paciente-->
+
+      <br> <!-- antecedentes Patologicos -->
+
+      <div class="row">
+        <div class="col bg-white p-3">
+          <!-- Botón que activa el panel -->
+          <button class="btn legend-button align-items-center" type="button" data-bs-toggle="collapse"
+            data-bs-target="#antecedentesPanel" aria-expanded="false" aria-controls="antecedentesPanel">
+            <span>Antecedentes Patológicos</span>
+            <i class="fas fa-angle-down icon-rotate" id="iconoColapso" style="font-size: 1.5rem;"></i>
+          </button>
+
+          <!-- Panel colapsable -->
+          <div class="collapse mt-3" id="antecedentesPanel">
+            <form id="pacienteForm">
+              <div class="row">
+                <div class="col-md-5 mb-3">
+                  <label for="diabetes" class="form-label">Padecimiento</label>
+                  <input type="text" class="form-control" id="diabetes" value="Diabetes" readonly>
+                </div>
+
+                <div class="col-md-1 mb-3">
+                  <label for="estadoCheckbox" class="form-label">Estado</label>
+                  <div class="form-check pt-2">
+                    <input type="checkbox" class="form-check-input" id="estadoCheckbox" style="transform: scale(1.5);"
+                      checked disabled>
+                  </div>
+                </div>
+
+                <div class="col-md-6 mb-3">
+                  <label for="descripcion" class="form-label">Descripción</label>
+                  <input type="text" class="form-control" id="descripcion" placeholder="Descripción" readonly>
+                </div>
+              </div>
+            </form>
           </div>
         </div>
       </div>
-
-      <div id="antecedentes" class="section">
-        <h2>Gestión de Antecedentes</h2>
-        <p>Contenido relacionado con los historiales aquí.</p>
-      </div>
     </div>
 
-    <!-- Scripts -->
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
-    <script>
-      function showSection(sectionId) {
-        // Ocultar todas las secciones
-        const sections = document.querySelectorAll('.section');
-        sections.forEach(section => section.classList.remove('active'));
+    <div id="antecedentes" class="section">
+      <h2>Gestión de Antecedentes</h2>
+      <p>Contenido relacionado con los historiales aquí.</p>
+    </div>
 
-        // Mostrar la sección seleccionada
-        const sectionToShow = document.getElementById(sectionId);
-        sectionToShow.classList.add('active');
-      }
-    </script>
+  </div>
+
+  <script src="script.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
---
+++ Pagina/pages/script.js
@@ -0,0 +1,50 @@
+const sidebar = document.querySelector(".sidebar");
+const sidebarClose = document.querySelector("#sidebar-close");
+const menu = document.querySelector(".menu-content");
+const menuItems = document.querySelectorAll(".submenu-item");
+const subMenuTitles = document.querySelectorAll(".submenu .menu-title");
+
+sidebarClose.addEventListener("click", () => sidebar.classList.toggle("close"));
+
+menuItems.forEach((item, index) => {
+  item.addEventListener("click", () => {
+    menu.classList.add("submenu-active");
+    item.classList.add("show-submenu");
+    menuItems.forEach((item2, index2) => {
+      if (index !== index2) {
+        item2.classList.remove("show-submenu");
+      }
+    });
+  });
+});
+
+subMenuTitles.forEach((title) => {
+  title.addEventListener("click", () => {
+    menu.classList.remove("submenu-active");
+  });
+});
+
+function showSection(sectionId) {
+  // Ocultar todas las secciones
+  const sections = document.querySelectorAll(".section");
+  sections.forEach((section) => section.classList.remove("active"));
+
+  // Mostrar la sección seleccionada
+  const sectionToShow = document.getElementById(sectionId);
+  sectionToShow.classList.add("active");
+}
+
+const collapseElement = document.getElementById("antecedentesPanel");
+const icono = document.getElementById("iconoColapso");
+
+// Evento cuando el panel se expande
+collapseElement.addEventListener("show.bs.collapse", () => {
+  icono.classList.add("rotate-up"); // Añade rotación
+});
+
+// Evento cuando el panel se contrae
+collapseElement.addEventListener("hide.bs.collapse", () => {
+  icono.classList.remove("rotate-up"); // Quita rotación
+});
+
+console.log(menuItems, subMenuTitles);
